0e30e598a9c24697b1998ca0e26060db1d89eb0e
darius <39945228+duxy6@users.noreply.github.com>
2023-03-30 02:32:38 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'DuXinYu',
+      id: 'oo2i55mQmkRQ7rsQmB1d0mKqSWBU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'oo2i55mQmkRQ7rsQmB1d0mKqSWBU',
+      useTemplateId: 'Ff56OsVm98aWFT0lNuPuJp4i5yRVoMd6BuY_v53gJR4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-15',
       festivals: [
