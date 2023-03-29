6d2ed73292e09f7d6baaeb834c38f17c8c608514
darius <39945228+duxy6@users.noreply.github.com>
2023-03-30 01:54:00 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,10 +8,10 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: 'wx313d2f0aa3fac87c',
+  APP_ID: '1000002',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '0b339c47672a9a922db9ddb52a8d64af',
+  APP_SECRET: 'SS9yOWpaHc6ZpREKoXgfzf1y5sb7K3oEDYT6k2MrKYQ',
 
   PROVINCE: '内蒙古',
   CITY: '包头',
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oo2i55mQmkRQ7rsQmB1d0mKqSWBU',
+      id: 'DuXinYu',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'Ff56OsVm98aWFT0lNuPuJp4i5yRVoMd6BuY_v53gJR4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -57,7 +57,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oo2i55mQmkRQ7rsQmB1d0mKqSWBU',
+      id: 'DuXinYu',
     }
   ],
 
